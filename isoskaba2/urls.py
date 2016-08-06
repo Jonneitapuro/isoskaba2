dcea02c94952598aede08e70916b4e06a66d1d5a
Jonneitapuro <jonne.i@hotmail.com>
2016-08-06 16:59:36 +0300

    show text on index page

--- isoskaba2/urls.py
+++ isoskaba2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from skaba import views as skabaviews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', skabaviews.index, name='index')
 ]
--- skaba/views.py
+++ skaba/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.template.response import TemplateResponse
+
 
 # Create your views here.
+
+def index(request):
+    response = TemplateResponse(request, 'index.html', {})
+    response.render()
+    return response
