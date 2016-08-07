a79e86d235b7b02c10bcb4087921655990c0abe6
Onni Lampi <contact@onnilampi.fi>
2016-08-07 20:44:50 +0300

    merge master

--- skaba/forms.py
+++ skaba/forms.py
@@ -4,40 +4,40 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class EventForm(forms.ModelForm):
-	class Meta:
-		model = Event
-		fields = ['name', 'description', 'slug', 'points', 'guild', 'repeats']
-	
-	name = forms.CharField(label='Event name', max_length=128, min_length=1)
-	description = forms.CharField(label='Description')
-	guild = forms.ModelChoiceField(queryset=Guild.objects.all(), empty_label=None)
-	repeats = forms.IntegerField(label='Repeats', initial=1, min_value=0)
-	points = forms.IntegerField(label='Points', min_value=0)
+        class Meta:
+                model = Event
+                fields = ['name', 'description', 'slug', 'points', 'guild', 'repeats']
+        
+        name = forms.CharField(label='Event name', max_length=128, min_length=1)
+        description = forms.CharField(label='Description')
+        guild = forms.ModelChoiceField(queryset=Guild.objects.all(), empty_label=None)
+        repeats = forms.IntegerField(label='Repeats', initial=1, min_value=0)
+        points = forms.IntegerField(label='Points', min_value=0)
 
 class AddUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
-	first_name = forms.CharField(required = True)
-    last_name = forms.CharField(required = True)
-	role = forms.CharField(label='User\'s role')
-	guild = forms.ModelChoiceField(queryset=Guild.objects.all(), empty_label=None)
-	is_kv = forms.BooleanField()
-	is_tf = forms.BooleanField()
+        email = forms.EmailField(required=True)
+        first_name = forms.CharField(required = True)
+        last_name = forms.CharField(required = True)
+        role = forms.CharField(label='User\'s role')
+        guild = forms.ModelChoiceField(queryset=Guild.objects.all(), empty_label=None)
+        is_kv = forms.BooleanField()
+        is_tf = forms.BooleanField()
 
-	class Meta:
-        model = User
-        fields = ('email', 'password1')
+        class Meta:
+            model = User
+            fields = ('email', 'password1')
 
-    def save(self,commit = True):
-        user = super(AddUserForm, self).save(commit = False)
-        user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first name']
-        user.last_name = self.cleaned_data['last name']
-		user.role = self.cleaned_data['role']
-		user.guild = self.cleaned_data['guild']
-		user.is_kv = self.cleaned_data['is_kv']
-		user.is_tf = self.cleaned_data['is_tf']
-
-
-        if commit:
-            user.save()
-			return user
+#        def save(self,commit = True):
+#            user = super(AddUserForm, self).save(commit = False)
+#            user.email = self.cleaned_data['email']
+#            user.first_name = self.cleaned_data['first name']
+#            user.last_name = self.cleaned_data['last name']
+#            user.role = self.cleaned_data['role']
+#            user.guild = self.cleaned_data['guild']
+#            user.is_kv = self.cleaned_data['is_kv']
+#            user.is_tf = self.cleaned_data['is_tf']
+#
+#
+#            if commit:
+#                user.save()
+#                return user
--- skaba/views.py
+++ skaba/views.py
@@ -42,87 +42,88 @@ def admin_index(request):
 
 @staff_member_required
 def event_add(request):
-	if request.method == 'POST':
-		form = EventForm(request.POST)
-		if (form.is_valid()):
-			try:
-				form.save()
-				status = 200
-				messages.add_message(request, messages.INFO, 'event added')
-				return redirect('/admin/events/add/')
-			except:
-				status = 400
+        if request.method == 'POST':
+                form = EventForm(request.POST)
+                if (form.is_valid()):
+                        try:
+                                form.save()
+                                status = 200
+                                messages.add_message(request, messages.INFO, 'event added')
+                                return redirect('/admin/events/add/')
+                        except:
+                                status = 400
 
-	else:
-		form = EventForm()
-		status = 200
+        else:
+                form = EventForm()
+                status = 200
 
-	token = {}
-	token.update(csrf(request))
-	token['form'] = form
-	token['site_title'] = 'Create event'
-	token['submit_text'] = 'Add event'
-	token['form_action'] = '/admin/events/add/'
+        token = {}
+        token.update(csrf(request))
+        token['form'] = form
+        token['site_title'] = 'Create event'
+        token['submit_text'] = 'Add event'
+        token['form_action'] = '/admin/events/add/'
 
-	return render_to_response('admin_form.html', token)
+        return render_to_response('admin_form.html', token)
 
 def event_edit(request, event_slug):
-	event = get_object_or_404(Event, slug=event_slug)
-	if request.method == 'POST':
-		form = EventForm(request.POST, instance=event)
-		if (form.is_valid()):
-			try:
-				form.save()
-				status = 200
-				messages.add_message(request, messages.INFO, 'event saved')
-				return redirect('/admin/events/edit/' + slug + '/')
-			except:
-				status = 400
+        event = get_object_or_404(Event, slug=event_slug)
+        if request.method == 'POST':
+                form = EventForm(request.POST, instance=event)
+                if (form.is_valid()):
+                        try:
+                                form.save()
+                                status = 200
+                                messages.add_message(request, messages.INFO, 'event saved')
+                                return redirect('/admin/events/edit/' + slug + '/')
+                        except:
+                                status = 400
 
-	else:
-		form = EventForm(instance=event)
-		status = 200
+        else:
+                form = EventForm(instance=event)
+                status = 200
 
-	token = {}
-	token.update(csrf(request))
-	token['form'] = form
-	token['site_title'] = 'Edit event'
-	token['submit_text'] = 'Save event'
-	token['form_action'] = '/admin/events/edit/' + event.slug + '/'
+        token = {}
+        token.update(csrf(request))
+        token['form'] = form
+        token['site_title'] = 'Edit event'
+        token['submit_text'] = 'Save event'
+        token['form_action'] = '/admin/events/edit/' + event.slug + '/'
 
-	return render_to_response('admin_form.html', token)
+        return render_to_response('admin_form.html', token)
 
 @staff_member_required
 def guilds_populate(request):
-	if Guild.objects.all().exists():
-		return redirect('index')
-	guilds = [
-		{'name': 'Yleinen', 'abbr': 'Yleinen'},
-		{'name': 'Arkkitehtikilta', 'abbr': 'AK'},
-		{'name': 'AS-kilta', 'abbr': 'AS'},
-		{'name': 'Athene', 'abbr': 'Athene'},
-		{'name': 'Fyysikkokilta', 'abbr': 'FK'},
-		{'name': 'Inkubio', 'abbr': 'Bio'},
-		{'name': 'Koneinsinöörikilta', 'abbr': 'KIK'},
-		{'name': 'Maanmittarikilta', 'abbr': 'MK'},
-		{'name': 'Prodeko', 'abbr': 'Prodeko'},
-		{'name': 'Prosessiteekkarit', 'abbr': 'PT'},
-		{'name': 'Rakennusinsinöörikilta', 'abbr': 'IK'},
-		{'name': 'Sähköinsinöörikilta', 'abbr': 'SIK'},
-		{'name': 'Tietokilta', 'abbr': 'TiK'},
-		{'name': 'Teknologföreningen', 'abbr': 'TF'}
-	]
-	for guild in guilds:
-		new_guild = Guild(name=guild['name'], abbreviation = guild['abbr'])
-		new_guild.save()
-	return redirect('index')
+        if Guild.objects.all().exists():
+                return redirect('index')
+        guilds = [
+                {'name': 'Yleinen', 'abbr': 'Yleinen'},
+                {'name': 'Arkkitehtikilta', 'abbr': 'AK'},
+                {'name': 'AS-kilta', 'abbr': 'AS'},
+                {'name': 'Athene', 'abbr': 'Athene'},
+                {'name': 'Fyysikkokilta', 'abbr': 'FK'},
+                {'name': 'Inkubio', 'abbr': 'Bio'},
+                {'name': 'Koneinsinöörikilta', 'abbr': 'KIK'},
+                {'name': 'Maanmittarikilta', 'abbr': 'MK'},
+                {'name': 'Prodeko', 'abbr': 'Prodeko'},
+                {'name': 'Prosessiteekkarit', 'abbr': 'PT'},
+                {'name': 'Rakennusinsinöörikilta', 'abbr': 'IK'},
+                {'name': 'Sähköinsinöörikilta', 'abbr': 'SIK'},
+                {'name': 'Tietokilta', 'abbr': 'TiK'},
+                {'name': 'Teknologföreningen', 'abbr': 'TF'}
+        ]
+        for guild in guilds:
+                new_guild = Guild(name=guild['name'], abbreviation = guild['abbr'])
+                new_guild.save()
+        return redirect('index')
 
 @staff_member_required
 def user_add(request):
-	if request.method == 'POST':
-		form = AddUserForm(request.POST)
-		if (form.is_valid()):
+        if request.method == 'POST':
+                form = AddUserForm(request.POST)
+                if (form.is_valid()):
                         form.save()
+                        messages.add_message(request, messages.INFO, 'Creation successfull')
                         return HttpResponseRedirect('/admin/users/add')
 #                        role = request.POST.get('role')
 #                        email = request.POST.get('email')
@@ -139,15 +140,18 @@ def user_add(request):
 #                                return redirect('/admin/users/add')
 #                        except:
 #                                status = 400
-
-        args = {}
-        args.update(csrf(request))
-        args['form'] = AddUserForm()
-        print args
-	token['form'] = form
-	token['site_title'] = 'Add User'
-	token['submit_text'] = 'Add user'
-	token['form_action'] = '/admin/users/add'
-        return render(request, 'admin_form.html', args)
-
-	#return render_to_response('admin_form.html', token)
+                else:
+                        messages.add_message(request, messages.INFO, 'Error in user creation')
+                        return HttpResponseRedirect('/admin/users/add')
+        else:
+            args = {}
+            args.update(csrf(request))
+            args['form'] = AddUserForm()
+            #print args
+            #args['form'] = form
+            args['site_title'] = 'Add User'
+            args['submit_text'] = 'Add user'
+            args['form_action'] = '/admin/users/add'
+            return render(request, 'admin_form.html', args)
+            #return HttpResponseRedirect('/admin/users/add')
+            #return render_to_response('admin_form.html', token)
