e3385fec2d3ed998b868f2e363ff535a6a3fe5fa
Jonneitapuro <jonne.i@hotmail.com>
2016-08-07 14:53:31 +0300

    add function to populate Guilds table

--- skaba/views.py
+++ skaba/views.py
@@ -5,6 +5,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.template.context_processors import csrf
 
 from skaba.forms import AddEventForm
+from skaba.models import Event, Guild
 
 # Create your views here.
 
@@ -47,3 +48,28 @@ def event_add(request):
 	token['form'] = form
 
 	return render_to_response('event_add.html', token)
+
+@staff_member_required
+def guilds_populate(request):
+	if Guild.objects.all().exists():
+		return redirect('index')
+	guilds = [
+		{'name': 'Yleinen', 'abbr': 'Yleinen'},
+		{'name': 'Arkkitehtikilta', 'abbr': 'AK'},
+		{'name': 'AS-kilta', 'abbr': 'AS'},
+		{'name': 'Athene', 'abbr': 'Athene'},
+		{'name': 'Fyysikkokilta', 'abbr': 'FK'},
+		{'name': 'Inkubio', 'abbr': 'Bio'},
+		{'name': 'Koneinsinöörikilta', 'abbr': 'KIK'},
+		{'name': 'Maanmittarikilta', 'abbr': 'MK'},
+		{'name': 'Prodeko', 'abbr': 'Prodeko'},
+		{'name': 'Prosessiteekkarit', 'abbr': 'PT'},
+		{'name': 'Rakennusinsinöörikilta', 'abbr': 'IK'},
+		{'name': 'Sähköinsinöörikilta', 'abbr': 'SIK'},
+		{'name': 'Tietokilta', 'abbr': 'TiK'},
+		{'name': 'Teknologföreningen', 'abbr': 'TF'}
+	]
+	for guild in guilds:
+		new_guild = Guild(name=guild['name'], abbreviation = guild['abbr'])
+		new_guild.save()
+	return redirect('index')
