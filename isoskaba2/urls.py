ad7e706cda1210736bcdbace6c33052bc241f68d
Jonneitapuro <jonne.i@hotmail.com>
2016-08-29 21:08:14 +0300

    import static to urls.py

--- isoskaba2/urls.py
+++ isoskaba2/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from skaba import views as skabaviews
+from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
 
