a479b6ef385d058fa0bcdca8681112edc51177bc
Jonneitapuro <Jonneitapuro@users.noreply.github.com>
2017-06-21 19:41:48 +0300

    echo descriptions when they start to run

--- test_applet.sh
+++ test_applet.sh
@@ -7,14 +7,14 @@ activate () {
 clear
 
 echo "This is a script that does the following things:"
-echo "* activate virtualenv"
-echo "* run Django migrations"
-echo "* start Django devserver"
-echo "* deactivate virtualenv"
 
+echo "* activate virtualenv"
 activate
+echo "* run Django migrations"
 python manage.py makemigrations --merge
 python manage.py makemigrations
 python manage.py migrate
+echo "* start Django devserver"
 python manage.py runserver
+echo "* deactivate virtualenv"
 deactivate
