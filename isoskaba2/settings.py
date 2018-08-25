af0682dac05b74712501e5c56d3d8ed7960db7d3
Elisa Naskali <elisa.naskali@gmail.com>
2018-08-25 20:42:45 +0300

    Added forgotten password retrieval

--- isoskaba2/settings.py
+++ isoskaba2/settings.py
@@ -156,3 +156,11 @@ MESSAGE_TAGS = {
 AUTH_PROFILE_MODULE = 'skaba.UserProfile'
 
 LOGIN_URL = '/login'
+
+#TODO: Add email host & password to local variables
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = EMAIL
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
