c4bbb3fbae6e1c60205f50d6b28465b812a98748
Elisa Naskali <elisa.naskali@gmail.com>
2018-10-23 19:13:54 +0300

    Added email information as env variables

--- isoskaba2/settings.py
+++ isoskaba2/settings.py
@@ -161,5 +161,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = EMAILUSER
-EMAIL_HOST_PASSWORD = EMAILPASSWORD
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
